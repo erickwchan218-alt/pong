6ab6aa828744dfcbc9e9cf7743867d48f8aaa8c5
Eric <erickwchan218@gmail.com>
2026-07-30 22:04:33 +0800

    "Just a test"

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream> 
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Hello World!" << std::endl;
+    }
     return 0;
 }
